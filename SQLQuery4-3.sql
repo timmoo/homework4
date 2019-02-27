ac9897bd7d4542fe8b11ff57793cfab5233177dc
timmoo <47295295+timmoo@users.noreply.github.com>
2019-02-27 21:37:14 +0300

    Add files via upload

---
+++ SQLQuery4-3.sql
@@ -0,0 +1,19 @@
+SELECT * FROM Sales.Customers
+WHERE CustomerID IN
+(SELECT TOP 5 CustomerID FROM Sales.CustomerTransactions 
+ORDER BY TransactionAmount desc);
+
+
+SELECT * FROM Sales.Customers
+WHERE CustomerID = SOME (SELECT TOP 5 CustomerID FROM Sales.CustomerTransactions ORDER BY TransactionAmount desc);
+ 
+
+WITH CustomerTransactionsCTE AS 
+(
+	SELECT TOP 5 CustomerID FROM Sales.CustomerTransactions 
+ORDER BY TransactionAmount desc
+)
+SELECT * 
+FROM Sales.Customers AS C
+	JOIN CustomerTransactionsCTE AS T
+		ON C.CustomerID = T.CustomerID;
