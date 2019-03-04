9b301068d208edb4219203db3475fe46502a635a
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-04 21:46:35 +0300

    Delete SQLQuery4-2.sql

--- SQLQuery4-2.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM Warehouse.StockItems
-WHERE UnitPrice <= ALL (SELECT UnitPrice 
-FROM Warehouse.StockItems);
-
-SELECT * 
-FROM Warehouse.StockItems 
-WHERE UnitPrice = (SELECT MIN(UnitPrice) 
-FROM Warehouse.StockItems)
